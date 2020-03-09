82a492fc2d0ecd5eaf4056e3aa5f9c686699c083
Amy Tseng <chinwentsengtw@gmail.com>
2020-03-10 00:03:34 +0800

    master and detail

--- insta/views.py
+++ insta/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-from django.views.generic.base import TemplateView
+from django.views.generic import DetailView, ListView, TemplateView
+
+from insta.models import Post
 # Create your views here.
 class Helloworld(TemplateView):
         template_name = 'test.html'
+
+class PostView(ListView):
+    model = Post
+    template_name = 'index.html'
